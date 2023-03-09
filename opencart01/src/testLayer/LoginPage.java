7f24a63e52aa190335257e69499f8d064a42ebc9
urmi-tekale <127080372+urmi-tekale@users.noreply.github.com>
2023-03-09 11:53:56 +0530

    login feature

---
+++ opencart01/src/testLayer/LoginPage.java
@@ -0,0 +1,10 @@
+package testLayer;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		System.out.println("these is my changed login feature class");
+
+	}
+
+}
--- opencart01/src/testPackage/TestClass.java
+++ opencart01/src/testPackage/TestClass.java
@@ -3,8 +3,9 @@ package testPackage;
 public class TestClass {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Test base class");
-		System.out.println("Utilities");
+		System.out.println("Utilities class");
 		System.out.println("pom.xml");
 	}
 
